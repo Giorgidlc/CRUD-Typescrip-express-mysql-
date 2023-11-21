81f2bf8a7d0ce8e0b5123aac7ffe740384649cf6
Jorge <jorestuard@gmail.com>
2023-11-21 15:55:41 +0100

    feat: fix models server with routes

--- models/server.ts
+++ models/server.ts
@@ -1,13 +1,25 @@
 import express, { Application } from 'express'
+import userRoutes from '../routes/user'
 
 class Server {
   private app: Application;
-  private port: string ;
+  private port: string;
+  private apiPaths = {
+    users: '/api/users'
+  }
   
   constructor() {
     this.app = express();
     this.port = process.env.PORT || '8000';
+    //definir mis rutas
+    this.routes();
   } 
+
+
+  routes() {
+    this.app.use(this.apiPaths.users, userRoutes)
+  }
+
   listen() {
     this.app.listen(this.port, () => {
       console.log(`Servidor corriendo en puerto: http://localhost:${this.port}`)
