97d23d4750a6df6ee6310e05b5f4daf57ca9151b
Jorge <jorestuard@gmail.com>
2023-11-23 17:52:36 +0100

    feat(create): creating controllers of crud

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,37 +1,59 @@
 import { Request, Response } from "express"
+import User from "../models/user"
 
-export const getUsers = (req: Request, res: Response) => {
-
-  res.json({
-    msg: 'getUsers'
-  })
-
+export const getUsers = async (req: Request, res: Response) => {
+  try { 
+    const users = await User.findAll();
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).send({ message: (error as Error).message });
+  }
 }
 
-export const getUser = (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
   const { id } = req.params;
-  res.json({
-    msg: 'getUser',
-    id
-  })
+  try {
+    const user = await User.findByPk(id);
+    if (!user) { 
+      res.status(404).json(`user with ${id} not found`)
+    } else {res.status(200).json({ user })} 
+  } catch (error) {
+    res.status(500).send({ message: (error as Error).message })
+  }
 }
 
-export const postUser = (req: Request, res: Response) => {
+export const postUser = async (req: Request, res: Response) => {
   const { body } = req;
-  res.json({
-    msg: 'postUser',
-    body
-  })
+  try {
+    const existingEmail = await User.findOne({
+      where: { email_user: body.email_user } });
+    if (existingEmail) {
+      res.status(400).json(`Email already exists: ${body.email_user}`)
+    }
+    const user = new User(body);
+    await user.save();
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).send({ message: (error as Error).message })
+  }
 }
 
-export const putUser = (req: Request, res: Response) => {
+export const putUser = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { body } = req;
-  res.json({
-    msg: 'putUser',
-    body,
-    id
-  })
+  const { body} = req;
+  try {
+    const user = User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({ message: `user with ${id} not found` });
+    } 
+    const instance: any = await user;
+    instance.update(body);
+    res.status(200).json({ user: instance });
+ 
+  } catch (error) {
+    res.status(500).send({ message: (error as Error).message })
+  }
+  
 }
 
 export const deleteUser = (req: Request, res: Response) => {
--- dist/controllers/user.js
+++ dist/controllers/user.js
@@ -1,37 +1,79 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUser = exports.putUser = exports.postUser = exports.getUser = exports.getUsers = void 0;
-const getUsers = (req, res) => {
-    res.json({
-        msg: 'getUsers'
-    });
-};
+const user_1 = __importDefault(require("../models/user"));
+const getUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const users = yield user_1.default.findAll();
+        res.status(200).json({ users });
+    }
+    catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
 exports.getUsers = getUsers;
-const getUser = (req, res) => {
+const getUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        msg: 'getUser',
-        id
-    });
-};
+    try {
+        const user = yield user_1.default.findByPk(id);
+        if (!user) {
+            res.status(404).json(`user with ${id} not found`);
+        }
+        else {
+            res.status(200).json({ user });
+        }
+    }
+    catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
 exports.getUser = getUser;
-const postUser = (req, res) => {
+const postUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { body } = req;
-    res.json({
-        msg: 'postUser',
-        body
-    });
-};
+    try {
+        const existingEmail = yield user_1.default.findOne({
+            where: { email_user: body.email_user }
+        });
+        if (existingEmail) {
+            res.status(400).json(`Email already exists: ${body.email_user}`);
+        }
+        const user = new user_1.default(body);
+        yield user.save();
+        res.status(201).json({ user });
+    }
+    catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
 exports.postUser = postUser;
-const putUser = (req, res) => {
+const putUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { body } = req;
-    res.json({
-        msg: 'putUser',
-        body,
-        id
-    });
-};
+    try {
+        const user = user_1.default.findByPk(id);
+        if (!user) {
+            return res.status(404).json({ message: `user with ${id} not found` });
+        }
+        const instance = yield user;
+        instance.update(body);
+        res.status(200).json({ user: instance });
+    }
+    catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
 exports.putUser = putUser;
 const deleteUser = (req, res) => {
     const { id } = req.params;
