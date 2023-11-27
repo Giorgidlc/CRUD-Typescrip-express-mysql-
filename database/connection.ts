91ac323834672f47cd4f04acaad285bcd3d4c0b3
Jorge <jorestuard@gmail.com>
2023-11-27 17:34:48 +0100

    fix .env file

--- database/connection.ts
+++ database/connection.ts
@@ -1,6 +1,9 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv'
+//carga archivo .env
+dotenv.config()
 
-const db = new Sequelize('crud', 'root', 'Jd011$$$', {
+const db = new Sequelize('crud', 'root', process.env.PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   //logging: false
--- dist/database/connection.js
+++ dist/database/connection.js
@@ -1,7 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('crud', 'root', 'Jd011$$$', {
+const dotenv_1 = __importDefault(require("dotenv"));
+//carga archivo .env
+dotenv_1.default.config();
+const db = new sequelize_1.Sequelize('crud', 'root', process.env.PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     //logging: false
